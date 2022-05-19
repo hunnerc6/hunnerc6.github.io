8bfbd61148ca7e31d4b06e8f08d14a657ad06243
Hunter Chrisman <58529558+hunnerc6@users.noreply.github.com>
2022-05-19 16:22:15 -0500

    Add files via upload

---
+++ test/pawfect/link/action_page.php
@@ -0,0 +1,7 @@
+<html>
+	<body>
+		Welcome <?php echo $_POST["firstname"]; ?><br>
+		Your email address is: <?php echo $_POST["uemail"]; ?><br>
+		The subject is: <?php echo $_POST["subject"]; ?><br>	
+	</body>
+</html>
